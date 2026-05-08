252fd206fc84577c130bcdf649a94fa752b36ecc
raghavendiran222222-cloud <raghavendiran222222@gmail.com>
2026-05-08 17:37:17 +0530

    feat: add module input variables and local value definitions

---
+++ envs/prod/locals.tf
@@ -0,0 +1,16 @@
+locals {
+  common_tags = merge(var.tags, {
+    ManagedBy    = "Terraform"
+    CreationDate = formatdate("MMDDYYYY", timestamp())
+  })
+
+  vnet_name    = "vnet-bdt-mvp-${var.environment}-eus2-001"
+  nsg_app_name = "nsg-bdt-mvp-app-${var.environment}-eus2-001"
+  asp_name     = "asp-bdt-mvp-${var.environment}-eus2-001"
+  app_name     = "app-bdt-mvp-${var.environment}-eus2-001"
+  mysql_name   = "mysql-bdt-mvp-${var.environment}-eus2-001"
+  storage_name = "stbdtmvp${var.environment}eus2001"
+
+  subnet_app_name  = "snet-bdt-app-${var.environment}-eus2-001"
+  subnet_data_name = "snet-bdt-data-${var.environment}-eus2-001"
+}
