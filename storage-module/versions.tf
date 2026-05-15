537e2dcd3c848818ea9dce2171630f39164dfc9b
raghavendiran222222-cloud <raghavendiran222222@gmail.com>
2026-05-15 16:06:18 +0530

    chore: tighten .gitignore and relax provider version constraints

--- storage-module/versions.tf
+++ storage-module/versions.tf
@@ -1,14 +1,10 @@
 terraform {
-  required_version = ">= 1.9.0, < 2.0.0"
+  required_version = ">= 1.14.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.71"
-    }
-    azapi = {
-      source  = "Azure/azapi"
-      version = "~> 2.9"
+      version = ">= 4.70.0"
     }
   }
 }
